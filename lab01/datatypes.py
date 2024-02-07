1904059d689945f6b82d19a78f9424bf9331551d
pabvlow <157787992+pabvlow@users.noreply.github.com>
2024-02-07 21:59:19 +0600

    labs

---
+++ lab01/datatypes.py
@@ -0,0 +1,34 @@
+#exercise 1
+x = 5
+print(type(x))
+#<class 'int'>
+
+#exercise 2
+x = "Hello World"
+print(type(x))
+#<class 'str'>
+
+#exercise 3
+x = 20.5
+print(type(x))
+#<class 'float'>
+
+#exercise 4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#<class 'list'>
+
+#exercise 5
+x = ("apple", "banana", "cherry")
+print(type(x))
+#<class 'tuple'>
+
+#exercise 6
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#<class 'dict'>
+
+#exercise 7
+x = True
+print(type(x))
+#<class 'bool'>
---
+++ lab03/classes.py
@@ -0,0 +1,24 @@
+#exercise 1
+
+class MyClass:
+    x = 5
+
+#exercise 2
+    
+class MyClass:
+    x = 5
+p1 = MyClass()
+
+#exercise 3
+
+class MyClass:
+    x = 5
+p1 = MyClass()
+print(p1.x)
+
+#exercise 4
+
+class Person:
+    def init(self, name, age):
+       self.name = name
+       self.age = age
